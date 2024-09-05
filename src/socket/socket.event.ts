1b8ef0eac2cb4181fbdc6218045cb7e5966900a0
devbu <emmanuelniasse@gmail.com>
2024-09-05 19:09:08 +0200

    feat: chat

--- src/socket/socket.event.ts
+++ src/socket/socket.event.ts
@@ -1,36 +1,81 @@
+import { JwtService } from "@nestjs/jwt";
 import {
-  ConnectedSocket,
-  MessageBody,
-  SubscribeMessage,
-  WebSocketGateway,
-  WebSocketServer,
-} from '@nestjs/websockets';
-import { Server, Socket } from 'socket.io';
+    ConnectedSocket,
+    MessageBody,
+    SubscribeMessage,
+    WebSocketGateway,
+    WebSocketServer,
+} from "@nestjs/websockets";
+import { Server, Socket } from "socket.io";
+import { UserService } from "../user/user.service";
 
 @WebSocketGateway({
-  cors: {
-    origin: '*',
-    methods: ['GET', 'POST'],
-  },
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"],
+    },
 })
 export class SocketEvent {
-  @WebSocketServer()
-  server: Server;
+    @WebSocketServer()
+    server: Server;
 
-  handleConnection(client: Socket) {
-    console.log('Client connected: ' + client.id);
-  }
+    private userSocketMap = new Map<string, string>(); // Map for storing userId to socketId
 
-  handleDisconnect(client: Socket) {
-    console.log('Client disconnected: ' + client.id);
-  }
+    constructor(
+        private readonly userService: UserService,
+        private jwtService: JwtService
+    ) {}
 
-  @SubscribeMessage('message')
-  handleEvent(
-    @MessageBody() data: string,
-    @ConnectedSocket() client: Socket,
-  ): string {
-    this.server.emit('message', client.id, data);
-    return data;
-  }
+    @SubscribeMessage("authenticate")
+    async handleAuthentication(
+        @MessageBody() authToken: string,
+        @ConnectedSocket() client: Socket
+    ) {
+        try {
+            // Verify token and get userId
+            const payload = this.jwtService.verify(authToken);
+            const userId = payload.id;
+
+            // Check if user exists
+            const user = await this.userService.user({ id: userId });
+
+            if (user) {
+                this.userSocketMap.set(userId, client.id);
+                console.log(
+                    `User ${userId} connected with socket ID ${client.id}`
+                );
+                // Emit success event to client
+                client.emit("authenticated");
+            } else {
+                client.disconnect();
+            }
+        } catch (error) {
+            console.error("Authentication failed:", error);
+            client.disconnect();
+        }
+    }
+
+    @SubscribeMessage("message")
+    async handleEvent(
+        @MessageBody() data: string,
+        @ConnectedSocket() client: Socket
+    ): Promise<string> {
+        const userId = Array.from(this.userSocketMap.entries()).find(
+            ([id, socketId]) => socketId === client.id
+        )?.[0];
+
+        if (userId) {
+            const user = await this.userService.user({ id: userId });
+            if (user) {
+                console.log("User info:", user.name);
+                this.server.emit("message", user.name, data);
+            } else {
+                console.log("User not found for ID:", userId);
+            }
+        } else {
+            console.log("Socket ID not associated with any user");
+        }
+
+        return data;
+    }
 }
--- src/socket/socket.module.ts
+++ src/socket/socket.module.ts
@@ -1,7 +1,9 @@
 import { Module } from "@nestjs/common";
+import { UserModule } from "src/user/user.module";
 import { SocketEvent } from "./socket.event";
 
 @Module({
     providers: [SocketEvent],
+    imports: [UserModule],
 })
 export class SocketModule {}
