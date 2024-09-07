73fd6573492f87b846bfc747dc9acc2574675f61
devbu <emmanuelniasse@gmail.com>
2024-09-08 00:22:38 +0200

    fix: prisma client path

--- src/rooms/rooms.service.ts
+++ src/rooms/rooms.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from "@nestjs/common";
 import { Room } from "@prisma/client";
-import { PrismaService } from "src/prisma.service";
+import { PrismaService } from "../prisma.service";
 
 @Injectable()
 export class RoomsService {
