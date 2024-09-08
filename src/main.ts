2f5658037e940930d0b8180d6c6592fcdb91587a
devbu <emmanuelniasse@gmail.com>
2024-09-08 17:29:10 +0200

    fix: cors origin

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { AppModule } from "./app.module";
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.enableCors({
-        origin: process.env.CLIENT_URL,
+        origin: "https://yadetout-client.vercel.app",
         methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
         credentials: true,
     });
