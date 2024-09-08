1134515cfe27ae08503d40e63f29736d66f31b96
devbu <emmanuelniasse@gmail.com>
2024-09-08 14:34:08 +0200

    remove sayhello

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,4 @@
-import { Controller, Get } from "@nestjs/common";
+import { Controller } from "@nestjs/common";
 
 @Controller()
-export class AppController {
-    constructor() {}
-
-    @Get()
-    sayHello(): string {
-        return "Hello";
-    }
-}
+export class AppController {}
