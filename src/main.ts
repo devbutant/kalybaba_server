800fc771290f3a560465062cd7a307ecf32ad614
devbu <emmanuelniasse@gmail.com>
2024-09-08 17:18:03 +0200

    chore: enable cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,11 @@ import { AppModule } from "./app.module";
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
-    app.enableCors();
+    app.enableCors({
+        origin: process.env.CLIENT_URL,
+        methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+        credentials: true,
+    });
     app.useGlobalPipes(new ValidationPipe());
 
     const config = new DocumentBuilder()
