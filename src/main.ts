655727b45bc87f1091dcdc0da9f505498cbdd658
devbu <emmanuelniasse@gmail.com>
2024-10-20 17:19:17 +0200

    wip: fix/cookie-parser

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { ValidationPipe } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
 import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
-import cookieParser from "cookie-parser";
+import * as cookieParser from "cookie-parser";
 import { AppModule } from "./app.module";
 
 async function bootstrap() {
