ef68c32c013cb4fde2b1bd1821a053244d8ad688
devbu <emmanuelniasse@gmail.com>
2024-10-23 21:14:38 +0200

    wip: domain env variable added

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -81,7 +81,7 @@ export class UserController {
                 sameSite:
                     process.env.NODE_ENV === "production" ? "none" : "strict",
                 maxAge: 60 * 60 * 1000, // 1 hour
-                domain: process.env.CLIENT_URL,
+                domain: process.env.CLIENT_DOMAIN,
             });
 
             return updatedUser;
