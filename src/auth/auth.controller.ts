44d9b0d9f23673a9dc240bd376e39e4c7fd270f1
devbu <emmanuelniasse@gmail.com>
2024-10-27 13:02:46 +0100

    sameSite none

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -31,7 +31,7 @@ export class AuthController {
         res.cookie("access_token", access_token, {
             httpOnly: true,
             secure: process.env.NODE_ENV === "production",
-            sameSite: "lax",
+            sameSite: "none",
             maxAge: 60 * 60 * 1000, // 1 hour
         });
 
@@ -45,7 +45,7 @@ export class AuthController {
         res.clearCookie("access_token", {
             httpOnly: true,
             secure: process.env.NODE_ENV === "production",
-            sameSite: "lax",
+            sameSite: "none",
             // sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
         });
         return "Logout successful";
