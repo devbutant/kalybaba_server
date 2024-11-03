108eea8fc5eb7bcc0ffe036c048a46180dae18d5
devbu <emmanuelniasse@gmail.com>
2024-11-03 21:06:22 +0100

    fix: SameSite

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -31,7 +31,8 @@ export class AuthController {
         res.cookie("access_token", access_token, {
             httpOnly: true,
             secure: process.env.NODE_ENV === "production",
-            sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+            sameSite: "lax",
+            // sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
             maxAge: 60 * 60 * 1000, // 1 hour
         });
 
@@ -45,7 +46,8 @@ export class AuthController {
         res.clearCookie("access_token", {
             httpOnly: true,
             secure: process.env.NODE_ENV === "production",
-            sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+            sameSite: "lax",
+            // sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
             maxAge: 60 * 60 * 1000, // 1 hour
         });
         return "Logout successful";
